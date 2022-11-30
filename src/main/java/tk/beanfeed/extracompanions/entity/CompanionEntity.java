576ac9c9d91fe2ea3be4ef48360b53403188e80a
BeanFeed <two.austin.two@gmail.com>
2022-11-30 16:00:30 -0500

    Update

--- src/main/java/tk/beanfeed/extracompanions/entity/CompanionEntity.java
+++ src/main/java/tk/beanfeed/extracompanions/entity/CompanionEntity.java
@@ -6,13 +6,24 @@ import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.WanderAroundGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -25,6 +36,7 @@ import software.bernie.geckolib3.util.GeckoLibUtil;
 import tk.beanfeed.extracompanions.ExtraCompanions;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class CompanionEntity extends PathAwareEntity implements IAnimatable {
@@ -61,12 +73,12 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
         super.writeCustomDataToNbt(nbt);
         nbt.putUuid("Owner", this.getMasterUUID());
         nbt.putBoolean("isWaiting", isWaiting);
-        nbt.putIntArray("stationPos", new int[]{this.station.getX(), this.station.getY(), this.station.getZ()});
+        if(this.station != null) nbt.putIntArray("stationPos", new int[]{this.station.getX(), this.station.getY(), this.station.getZ()});
     }
 
     public void readCustomDataFromNbt(NbtCompound nbt) {
         super.readCustomDataFromNbt(nbt);
-        if(nbt.containsUuid("Owner")) this.master = this.world.getPlayerByUuid(nbt.getUuid("Owner"));
+        if(nbt.containsUuid("Owner")) setMaster(this.world.getPlayerByUuid(nbt.getUuid("Owner")));
         if(nbt.contains("isWaiting")) this.isWaiting = nbt.getBoolean("isWaiting");
         if(nbt.contains("stationPos")) {
             int[] stationPos = nbt.getIntArray("stationPos");
@@ -95,7 +107,7 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
     public void setMaster(UUID uuid) {
         this.master = this.world.getPlayerByUuid(uuid);
     }
-
+    public void leaveMaster() { this.master = null;}
     public UUID getMasterUUID() {
         return this.master.getUuid();
     }
@@ -107,6 +119,54 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
 
     private boolean isMoving() {
         return !(this.getVelocity().x == 0 && this.getVelocity().z == 0);
+    }
+
+    public ActionResult interact(PlayerEntity player, Hand hand) {
+        if (!this.isAlive()) {
+            return ActionResult.PASS;
+        } else if (this.getHoldingEntity() == player) {
+            this.detachLeash(true, !player.getAbilities().creativeMode);
+            return ActionResult.success(this.world.isClient);
+        } else {
+            ActionResult actionResult = this.interactWithItem(player, hand);
+            if (actionResult.isAccepted()) {
+                return actionResult;
+            } else {
+                actionResult = this.interactMob(player, hand);
+                if (actionResult.isAccepted()) {
+                    this.emitGameEvent(GameEvent.ENTITY_INTERACT);
+                    return actionResult;
+                } else {
+                    return super.interact(player, hand);
+
+                }
+            }
+        }
+    }
+
+    private ActionResult interactWithItem(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (itemStack.isOf(Items.LEAD) && this.canBeLeashedBy(player)) {
+            this.attachLeash(player, true);
+            itemStack.decrement(1);
+            return ActionResult.success(this.world.isClient);
+        } else if (itemStack.isOf(Items.NAME_TAG)) {
+                ActionResult actionResult = itemStack.useOnEntity(player, this, hand);
+                if (actionResult.isAccepted()) {
+                    return actionResult;
+                }
+        } else if(itemStack.isOf(Items.STICK)) {
+            this.setMaster(player);
+            if(player instanceof ServerPlayerEntity pl) pl.sendMessage(Text.of("This Companion Is Now Yours"));
+            return ActionResult.SUCCESS;
+        } else if(itemStack.isOf(Items.BLAZE_ROD)) {
+            this.leaveMaster();
+            if(player instanceof ServerPlayerEntity pl) pl.sendMessage(Text.of("This Companion Is No Longer Yours"));
+            return ActionResult.SUCCESS;
+        }
+
+
+        return ActionResult.PASS;
     }
 
     public void tick() {
