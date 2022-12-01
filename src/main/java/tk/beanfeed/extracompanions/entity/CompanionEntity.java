c7b5fdf849d8968afc60a55e67ac58ebe36f6c2f
BeanFeed <two.austin.two@gmail.com>
2022-12-01 13:47:31 -0500

    Update FollowMasterGoal and CompanionEntity

--- src/main/java/tk/beanfeed/extracompanions/entity/CompanionEntity.java
+++ src/main/java/tk/beanfeed/extracompanions/entity/CompanionEntity.java
@@ -2,6 +2,7 @@ package tk.beanfeed.extracompanions.entity;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.WanderAroundGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
@@ -34,6 +35,7 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 import tk.beanfeed.extracompanions.ExtraCompanions;
+import tk.beanfeed.extracompanions.entity.ai.goal.FollowMasterGoal;
 
 import java.util.List;
 import java.util.Optional;
@@ -47,6 +49,13 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
     private BlockPos station;
 
     private boolean isWaiting = false;
+    private boolean isFollowing = false;
+
+    private NbtCompound persistantData = new NbtCompound();
+
+    private String pDatS = "extracompanions.compData";
+
+    private WanderAroundGoal wag = new WanderAroundGoal(this, speed, 5);
 
     private static final float speed = 4f / 10f;
     protected CompanionEntity(EntityType<CompanionEntity> entityType, World world) {
@@ -54,7 +63,8 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
     }
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new WanderAroundGoal(this, speed, 5));
+        this.goalSelector.add(1, new FollowMasterGoal(this, speed, 14));
+        this.goalSelector.add(2, wag);
     }
     public static DefaultAttributeContainer.Builder setAttributes() {
 
@@ -70,22 +80,34 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
     }
 
     public void writeCustomDataToNbt(NbtCompound nbt) {
+        if(persistantData == null) persistantData = new NbtCompound();
+        nbt.put(pDatS, persistantData);
+        if(this.master != null) persistantData.putUuid("Owner", this.getMasterUUID());
+        persistantData.putBoolean("isWaiting", isWaiting);
+        if(this.station != null) persistantData.putIntArray("stationPos", new int[]{this.station.getX(), this.station.getY(), this.station.getZ()});
         super.writeCustomDataToNbt(nbt);
-        nbt.putUuid("Owner", this.getMasterUUID());
-        nbt.putBoolean("isWaiting", isWaiting);
-        if(this.station != null) nbt.putIntArray("stationPos", new int[]{this.station.getX(), this.station.getY(), this.station.getZ()});
     }
 
     public void readCustomDataFromNbt(NbtCompound nbt) {
-        super.readCustomDataFromNbt(nbt);
-        if(nbt.containsUuid("Owner")) setMaster(this.world.getPlayerByUuid(nbt.getUuid("Owner")));
-        if(nbt.contains("isWaiting")) this.isWaiting = nbt.getBoolean("isWaiting");
-        if(nbt.contains("stationPos")) {
-            int[] stationPos = nbt.getIntArray("stationPos");
-            this.station = new BlockPos(stationPos[0], stationPos[1], stationPos[2]);
+        if(nbt.contains(pDatS)) persistantData = nbt.getCompound(pDatS);
+        if(persistantData != null)
+        {
+            if(persistantData.containsUuid("Owner")) setMaster(this.world.getPlayerByUuid(persistantData.getUuid("Owner")));
+            if(persistantData.contains("isWaiting")) this.isWaiting = nbt.getBoolean("isWaiting");
+            if(persistantData.contains("stationPos")) {
+                int[] stationPos = nbt.getIntArray("stationPos");
+                this.station = new BlockPos(stationPos[0], stationPos[1], stationPos[2]);
+            }
         }
+
+        super.readCustomDataFromNbt(nbt);
     }
-        @Override
+
+    public NbtCompound getPersistantData() {
+        if(this.persistantData == null) return new NbtCompound();
+        return this.persistantData;
+    }
+    @Override
     public void registerControllers(AnimationData data) {
         AnimationController<CompanionEntity> idleCon = new AnimationController<CompanionEntity>(this, "idleController", 0, this::defPredicate);
         data.addAnimationController(idleCon);
@@ -102,21 +124,37 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
 
     public void setMaster(PlayerEntity player) {
         this.master = player;
+        persistantData.putUuid("Owner", this.getMasterUUID());
     }
 
     public void setMaster(UUID uuid) {
-        this.master = this.world.getPlayerByUuid(uuid);
-    }
-    public void leaveMaster() { this.master = null;}
-    public UUID getMasterUUID() {
-        return this.master.getUuid();
-    }
 
+        this.master = this.world.getPlayerByUuid(uuid);
+        persistantData.putUuid("Owner", uuid);
+    }
+    public void leaveMaster() {
+        this.master = null;
+        persistantData.putUuid("Owner", new UUID(0L, 0L));
+    }
+    public UUID getMasterUUID() {
+        return master.getUuid();
+    }
+    public boolean isWaiting() { return isWaiting; }
+    public boolean isFollowing() { return isFollowing; }
     private boolean isMoving(boolean includeY) {
         if(includeY) return this.getVelocity().x == 0 && this.getVelocity().z == 0 && this.getVelocity().y == 0;
         return this.getVelocity().x == 0 && this.getVelocity().z == 0;
     }
-
+    public void setFollowing() {
+        this.isFollowing = !this.isFollowing;
+        if(isFollowing && isWaiting) setWaiting();
+    }
+    public void setWaiting() {
+        this.isWaiting = !this.isWaiting;
+        persistantData.putBoolean("isWaiting", this.isWaiting);
+        if(isWaiting) this.goalSelector.remove(wag);
+        else if(!this.goalSelector.getGoals().contains(wag)) this.goalSelector.add(2, wag);
+    }
     private boolean isMoving() {
         return !(this.getVelocity().x == 0 && this.getVelocity().z == 0);
     }
@@ -138,13 +176,13 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
                     return actionResult;
                 } else {
                     return super.interact(player, hand);
-
                 }
             }
         }
     }
 
     private ActionResult interactWithItem(PlayerEntity player, Hand hand) {
+
         ItemStack itemStack = player.getStackInHand(hand);
         if (itemStack.isOf(Items.LEAD) && this.canBeLeashedBy(player)) {
             this.attachLeash(player, true);
@@ -163,12 +201,15 @@ public class CompanionEntity extends PathAwareEntity implements IAnimatable {
             this.leaveMaster();
             if(player instanceof ServerPlayerEntity pl) pl.sendMessage(Text.of("This Companion Is No Longer Yours"));
             return ActionResult.SUCCESS;
-        }
+        }else if(itemStack.isEmpty() && getMaster() == player) { setWaiting(); return ActionResult.SUCCESS;}
+        else if(itemStack.isOf(Items.PRISMARINE_CRYSTALS)) { setFollowing(); return ActionResult.SUCCESS; }
 
 
         return ActionResult.PASS;
     }
 
+    @Override
+    public boolean canBeLeashedBy(PlayerEntity player) { return false; }
     public void tick() {
         super.tick();
         //ExtraCompanions.printOut(Boolean.toString(this.getVelocity().x == 0 && this.getVelocity().z == 0));
--- src/main/java/tk/beanfeed/extracompanions/entity/ai/goal/FollowMasterGoal.java
+++ src/main/java/tk/beanfeed/extracompanions/entity/ai/goal/FollowMasterGoal.java
@@ -1,13 +1,108 @@
 package tk.beanfeed.extracompanions.entity.ai.goal;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeavesBlock;
 import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
+import net.minecraft.entity.ai.pathing.PathNodeType;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
+import tk.beanfeed.extracompanions.Utils;
 import tk.beanfeed.extracompanions.entity.CompanionEntity;
 
 public class FollowMasterGoal extends Goal {
-
-    public FollowMasterGoal(CompanionEntity self){}
+    private CompanionEntity self;
+    private double speed;
+    private double maxDistance;
+    private EntityNavigation navigation;
+    private WorldView world;
+    private boolean movingToPlayer = false;
+    private boolean hasAlertedPlayer = false;
+    public FollowMasterGoal(CompanionEntity self, double speed, double maxDistance) {
+        this.self = self;
+        this.speed = speed;
+        this.maxDistance = maxDistance;
+        this.navigation = self.getNavigation();
+        this.world = self.world;
+    }
     @Override
     public boolean canStart() {
+        boolean isCloseAndIn = false;
+        if(self.world.getPlayerByUuid(self.getMasterUUID()) != null && !self.isWaiting() && self.isFollowing()) {
+            if (Utils.distanceTo(self.getBlockPos(), self.getMaster().getBlockPos()) < maxDistance) { isCloseAndIn = true; hasAlertedPlayer = false;}
+            else
+            {
+                if(tryTeleport()) return false;
+            }
+        }
+        if(!isCloseAndIn && !hasAlertedPlayer) { self.getMaster().sendMessage(Text.of("Companion Could Not Reach You")); hasAlertedPlayer = true;}
         return false;
     }
+
+    @Override
+    public boolean shouldContinue() {
+        return canStart();
+    }
+    @Override
+    public void stop() {
+        if(movingToPlayer) { this.navigation.stop(); movingToPlayer = false; }
+    }
+    @Override
+    public void tick() {
+        if(this.self.getMaster() != null && !this.self.isLeashed())
+        {
+            this.self.getLookControl().lookAt(this.self.getMaster(), 10.0f, (float)this.self.getMaxLookPitchChange());
+            if(Utils.distanceTo(this.self.getBlockPos(), this.self.getMaster().getBlockPos()) > 2 && shouldContinue())
+            {
+                this.navigation.startMovingTo(this.self.getMaster(), speed);
+                movingToPlayer = true;
+            }else
+            {
+                this.navigation.stop();
+                movingToPlayer = false;
+            }
+        }
+    }
+
+    private boolean tryTeleport() {
+        BlockPos blockPos = this.self.getMaster().getBlockPos();
+
+        for(int i = 0; i < 10; ++i) {
+            int j = this.getRandomInt(-3, 3);
+            int k = this.getRandomInt(-1, 1);
+            int l = this.getRandomInt(-3, 3);
+            boolean bl = this.tryTeleportTo(blockPos.getX() + j, blockPos.getY() + k, blockPos.getZ() + l);
+            if (bl) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+    private boolean tryTeleportTo(int x, int y, int z) {
+        if (Math.abs((double)x - this.self.getMaster().getX()) < 2.0 && Math.abs((double)z - this.self.getMaster().getZ()) < 2.0) {
+            return false;
+        } else if (!this.canTeleportTo(new BlockPos(x, y, z))) {
+            return false;
+        } else {
+            this.self.refreshPositionAndAngles((double)x + 0.5, (double)y, (double)z + 0.5, this.self.getYaw(), this.self.getPitch());
+            this.navigation.stop();
+            return true;
+        }
+    }
+    private boolean canTeleportTo(BlockPos pos) {
+        PathNodeType pathNodeType = LandPathNodeMaker.getLandNodeType(this.world, pos.mutableCopy());
+        if (pathNodeType != PathNodeType.WALKABLE) {
+            return false;
+        } else {
+            BlockPos blockPos = pos.subtract(this.self.getBlockPos());
+            return this.world.isSpaceEmpty(this.self, this.self.getBoundingBox().offset(blockPos));
+        }
+    }
+    private int getRandomInt(int min, int max) {
+        return this.self.getMaster().getRandom().nextInt(max - min + 1) + min;
+    }
+
 }
